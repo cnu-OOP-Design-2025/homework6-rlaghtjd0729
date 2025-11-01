8d07831f081ba6f460af43eff7e71a6f52ad00cf
rlaghtjd0729@o.cnu.ac.kr <master@gimhoseong-ui-MacBookAir.local>
2025-11-02 03:23:46 +0900

    과제제출

--- bird.cpp
+++ bird.cpp
@@ -1,3 +1,61 @@
 #include <iostream>
 #include "bird.h"
 
+// ******************************
+// Bird 클래스 구현
+// ******************************
+// Bird 클래스는 생성자/소멸자 외에는 순수 가상 함수를 사용하거나 기본 구현을 사용하므로,
+// 여기에 추가 구현은 필요하지 않습니다.
+
+// ******************************
+// Duck 클래스 구현
+// ******************************
+Duck::Duck() : Bird("Duck") {
+    cry(); 
+}
+
+Duck::~Duck() {
+    cry(); // 소멸자에서 cry() 호출
+}
+
+void Duck::cry() {
+    std::cout << "Quack!" << std::endl;
+}
+
+// ******************************
+// Penguin 클래스 구현
+// ******************************
+Penguin::Penguin() : Bird("Penguin") {
+    cry(); 
+}
+
+Penguin::~Penguin() {
+    cry(); // 소멸자에서 cry() 호출
+}
+
+void Penguin::cry() {
+    std::cout << "Squawk!" << std::endl;
+}
+
+void Penguin::fly() {
+    std::cout << "This bird can't fly." << std::endl; // 오버라이딩 구현
+}
+
+// ******************************
+// Eagle 클래스 구현
+// ******************************
+Eagle::Eagle() : Bird("Eagle") {
+    cry(); 
+}
+
+Eagle::~Eagle() {
+    cry(); // 소멸자에서 cry() 호출
+}
+
+void Eagle::cry() {
+    std::cout << "Screech!" << std::endl;
+}
+
+void Eagle::swim() {
+    std::cout << "This bird can't swim." << std::endl; // 오버라이딩 구현
+}
--- bird.h
+++ bird.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <iostream>
+#include <string> // std::string을 사용하기 위해 추가
 
 class Bird {
+protected: // 자식 클래스에서 접근할 수 있도록 private에서 protected로 변경
     std::string species; 
 public:
-    Bird(){}
+    // 기본 생성자는 제거합니다. (Bird는 추상 클래스로 사용되므로)
     Bird(std::string species): species(species){
         std::cout << species << " has borned" << std::endl;
     }
@@ -22,9 +24,36 @@ public:
         std::cout << "This bird can swim." << std::endl;
     }
 
-    virtual void cry() = 0;
+    virtual void cry() = 0; // 순수 가상 함수
 
-    virtual ~Bird() {
+    virtual ~Bird() { // 가상 소멸자 유지
         std::cout << species << " has died" << std::endl;
      }
+};
+
+// ------------------------------------
+// 자식 클래스 선언 (main.cpp가 인식할 수 있도록 추가)
+// ------------------------------------
+
+class Duck : public Bird {
+public:
+    Duck();
+    ~Duck() override;
+    void cry() override;
+};
+
+class Penguin : public Bird {
+public:
+    Penguin();
+    ~Penguin() override;
+    void cry() override;
+    void fly() override; // fly() 오버라이딩 선언
+};
+
+class Eagle : public Bird {
+public:
+    Eagle();
+    ~Eagle() override;
+    void cry() override;
+    void swim() override; // swim() 오버라이딩 선언
 };
